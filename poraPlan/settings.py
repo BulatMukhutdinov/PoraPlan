89ff2d9535d6bbbf6051dd53046189ba08eebdb0
Igor Bobko <limit-speed@yandex.ru>
2016-10-04 10:06:32 +0300

    SqlLIGHT

--- poraPlan/settings.py
+++ poraPlan/settings.py
@@ -87,10 +87,8 @@ DATABASES = {
     #     },
     # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
