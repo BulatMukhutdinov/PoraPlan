c1c5b7c16269033e1a2e313eb43be90eea59720a
Igor Bobko <limit-speed@yandex.ru>
2016-10-11 16:10:04 +0300

    MeetingList fix

--- meetings/views.py
+++ meetings/views.py
@@ -3,13 +3,11 @@ from meetings.models import Meeting,Agenda
 from django.core.urlresolvers import reverse_lazy
 
 from django.contrib.auth.decorators import permission_required
-
 # Create your views here.
-@permission_required('polls.can_vote')
+#@permission_required('polls.can_vote')
+
 class MeetingList(ListView):
-    def get_context_data(self, **kwargs):
-        context = super(MeetingList, self).get_context_data(**kwargs)
-        return context
+
     model = Meeting
 
 class MeetingCreate(CreateView):
