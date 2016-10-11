123ad450d284812f89f5a308bc8a7e7eab80d5de
Timur Fayzrakhmanov <timfayz.only@gmail.com>
2016-10-11 16:28:27 +0300

    Init Plan module

---
+++ place/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Place(models.Model):
+    room_number = models.IntegerField()
+    white_board = models.BooleanField()
+    black_board = models.BooleanField()
+    projector =   models.BooleanField()
+    start_date = models.DateTimeField()
+    duration = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxValueValidator(300)])
+    num_of_persons = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxValueValidator(30)])
+
