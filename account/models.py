cc7185f4d3455f7164f58d141d16e18c8fc30a2a
Stepanov Vyacheslav <v.stepanov@innopolis.ru>
2016-10-18 17:35:25 +0300

    Add account entity

--- account/models.py
+++ account/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateTimeField(null=True)
+    avatar = models.ImageField(null=True)
+    company = models.TextField(max_length=100)
+    position = models.TextField(max_length=100)
