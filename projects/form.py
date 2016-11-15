cc5c48f867365f366c473ad612c295de127f5295
Igor Bobko <limit-speed@yandex.ru>
2016-11-15 17:06:45 +0300

    Add some functionallity for creating projects

--- projects/form.py
+++ projects/form.py
@@ -1,4 +1,7 @@
+import datetime
 from django import forms
+from django.forms import widgets
+
 from projects.models import Project
 
 
@@ -7,8 +10,10 @@ class ProjectForm(forms.ModelForm):
         super(ProjectForm, self).__init__(*args, **kwargs)
         self.fields['name'].widget.attrs = {'class':'form-control','required':'required'}
         self.fields['description'] = forms.CharField(widget=forms.Textarea)
-        self.fields['description'].attrs = {'class':'form-control'}
+        self.fields['description'].widget.attrs = {'class':'form-control'}
+        self.fields['deadline'].widget.attrs = {'class': 'form-control'}
+        #self.fields['deadline'] = forms.DateField(initial=datetime.date.today)
 
     class Meta:
          model = Project
-         fields = ['name', 'description']
+         fields = ['name', 'description','deadline']
