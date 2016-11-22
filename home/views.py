4bc881056aca1fc80d2397a7395fbe5ac97fa51e
Timur Fayzrakhmanov <timfayz.only@gmail.com>
2016-11-22 18:15:36 +0300

    Hotfix for @login_required

--- home/views.py
+++ home/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-@login_required
+#@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
