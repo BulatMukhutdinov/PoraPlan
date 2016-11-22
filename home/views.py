0eb34ab3e4649502c03c4baf6b5e6184d15a0b38
Timur Fayzrakhmanov <timfayz.only@gmail.com>
2016-11-22 18:19:00 +0300

    Add @login_required to dashboard

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -6,6 +7,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-#@login_required
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
